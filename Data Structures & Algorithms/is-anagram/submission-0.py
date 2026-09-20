6777f45a6e931a010be124c231d541e1b217d6e9
Pradip Pokhrel <109421980+icarussfalls@users.noreply.github.com>
2026-09-20 13:50:59 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # edge case
+        if len(s) != len(t):
+            return False # cant be anagram if both length not same
+        # need two hashmap
+        countS = {}
+        countT = {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+        
